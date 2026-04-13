d86bc8ac00274882ec147d875b0e47aa34817cf2
Arthur Svinarenko <arthur.svinarenko@ab-soft.net>
2026-04-13 23:13:13 +0300

    crete security module

--- app/components/commission/Commission.tsx
+++ app/components/commission/Commission.tsx
@@ -10,12 +10,44 @@ export default function Commission () {
             <SectionLabel />
             <h2 className="t-center">Вы всегда знаете,<br></br> за что платите</h2>
             <div className={styles.commissionsContainer}>
-                <div className={styles.commissionItem}></div>
-                <div className={styles.commissionItem}></div>
-                <div className={styles.commissionItem}></div>
-                <div className={styles.commissionItem}></div>
-                <div className={styles.commissionItem}></div>
-                <div className={styles.commissionItem}></div>        
+                <div className={styles.commissionItem}>
+                    <h3>Выпуск<br></br> карты</h3>
+                    <div className={styles.commissionItemText}>
+                        <p>Единоразовая плата за мгновенную активацию вашей виртуальной Mastercard</p>
+                        <p className={styles.commissionPrice}>25$</p>
+                    </div>
+                </div>
+                <div className={styles.commissionItem}>
+                    <h3>Первый<br></br> депозит</h3>
+                    <div className={styles.commissionItemText}>
+                        <p>Минимальная сумма для старта — пополните рублями или криптой</p>
+                        <p className={styles.commissionPrice}><span>мин.</span> 25$</p>
+                    </div>
+                </div>
+                <div className={styles.commissionItem}>
+                    <h3>Комиссия<br></br> за пополнение</h3>
+                    <div className={styles.commissionItemText}>
+                        <p>Покрывает быстрое и безопасное зачисление средств</p>
+                        <p className={styles.commissionPrice}><span>фикс.</span> 4%</p>
+                    </div>
+                </div>
+                <div className={styles.commissionItem}>
+                    <h3>Комиссия<br></br> за транзакцию</h3>
+                    <div className={styles.commissionItemText}>
+                        <p>Небольшая плата за каждый платёж</p>
+                        <p className={styles.commissionPrice}>$0.25 <span>+.4%</span></p>
+                    </div>
+                </div>
+                <div className={clsx(styles.commissionItem, styles.commissionItemCard)}>
+                    <img src="/svg/Card.svg" alt="card" width={698} height={485} loading='lazy' decoding='async'/>
+                </div>
+                <div className={styles.commissionItem}>
+                    <h3>Ежемесячная<br></br> плата</h3>
+                    <div className={styles.commissionItemText}>
+                        <p>Никаких регулярных списаний — платите только за то, чем пользуетесь</p>
+                        <p className={styles.commissionPrice}>0$</p>
+                    </div>
+                </div>        
             </div>
         </section>
     );
