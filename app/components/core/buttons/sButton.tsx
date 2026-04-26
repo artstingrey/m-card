4ac98dcd0708ddbf9fe5744f8a195c74315f0174
Arthur Svinarenko <arthur.svinarenko@ab-soft.net>
2026-04-26 13:41:15 +0300

    remvoe button base

--- app/components/core/buttons/sButton.tsx
+++
@@ -1,7 +0,0 @@
-import { SButtonType } from "@/app/config/types";
-import SButtonBase from "./sButtonBase";
-
-
-export default function SButton (props: SButtonType) {
-    return <SButtonBase {...props} />;
-}
--- app/components/core/buttons/sButtonSmall.tsx
+++
@@ -1,6 +0,0 @@
-import { SButtonType } from "@/app/config/types";
-import SButtonBase from "./sButtonBase";
-
-export default function SButtonSmall (props: SButtonType) {
-    return <SButtonBase {...props} sizeClass="s-button--small" />;
-}
