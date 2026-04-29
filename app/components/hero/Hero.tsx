f206cd30a3f9bf5cb164c824c57c64087f778da9
Arthur Svinarenko <arthur.svinarenko@ab-soft.net>
2026-04-29 16:51:44 +0300

    test video tag

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -84,9 +84,9 @@ export default function Hero() {
             {/* <Image className={styles.heroBgMob} src="/images/hero-m.jpg"  alt="hero bg mobile" quality={100} loading="eager" sizes="100vw" fill priority/> */}
             
             {/* {src && <video autoPlay muted loop playsInline preload="auto" key={src}><source src={src} type="video/mp4" /></video>} */}
-            {src && <video autoPlay muted loop playsInline preload="auto" key={src}>
-                <source src={src} type="video/mp4" />
-            </video>}
+            <video autoPlay muted loop playsInline preload="auto" key={src}>
+                {src && <source src={src} type="video/mp4" />}
+            </video>
             {/* <Image className={styles.heroBg} src="/images/hero-b.jpg"  alt="hero bg" quality={100} loading="eager" width="3840" height="2924" priority/> */}
             
             <div className={clsx("m-container", styles.heroContainer)}>
