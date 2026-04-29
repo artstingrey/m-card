1fd6484fb3d251c9fcfc8b8192aa003bebf04ca9
Arthur Svinarenko <arthur.svinarenko@ab-soft.net>
2026-04-29 15:58:34 +0300

    update resize

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -57,7 +57,7 @@ export default function Hero() {
 
         const resizeFunc = () => {
             handleViewportChange();
-            //updateVideoSrc();
+            updateVideoSrc();
         };
 
         updateHeroButtonState();
